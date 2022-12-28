691f76549662e6394c9c28e7e01fe60d838f75e3
user1026 <2914153618@qq.com>
2022-12-29 00:22:20 +0800

    修改项目结构

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"gin01/src/DataBase"
+	"gin01/src/global"
 	"gin01/src/routes"
 )
 
 func main() {
 
-	DataBase.Init()
+	global.GLOBAL_DB = DataBase.InitDb()
+
+	DB := global.GLOBAL_DB.DB()
+	defer func() {
+		if err := DB.Close(); err != nil {
+			fmt.Println("数据库关闭失败")
+		}
+	}()
 	r := routes.Routes()
 	r.Run(":9000")
-	defer DataBase.Db.Close()
+
 }
